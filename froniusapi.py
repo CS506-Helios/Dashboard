b41cd634ea11055f6931ac42079b83ad788c8c8f
Ying <wang975@wisc.edu>
2017-11-22 20:37:42 -0600

    Add files via upload
    Need IP address of the inverter(from Fronius) to run the code.
    
    ref: http://www3.fronius.com/cps/rde/xbcr/SID-A761BF1D-F8F8FEE6/fronius_international/42_0410_2012_318486_snapshot.pdf

---
+++ froniusapi.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 19:27:47 2017
+
+@author: ying
+"""
+
+
+
+import requests
+import time
+
+# IP address of inverter
+host = ""
+# number of seconds between samples
+sample_seconds = 60 * 10
+
+
+def main():
+    print("started")
+    while True:
+        try:
+            watts = watts_generated()
+            now = time.strftime("%H:%M:%S")
+            line = "%s\t%s\n" % (now, watts)
+            # print(line)
+            write_to_logfile(line)
+        except requests.exceptions.ConnectTimeout:
+            print("Connect timeout at %s" % time.strftime("%H:%M:%S"))
+        if sample_seconds > 0:
+            time.sleep(sample_seconds)
+        else:
+            return
+
+
+def write_to_logfile(line):
+    today = time.strftime("%Y_%m_%d")
+    file_name = today + ".csv"
+    out_file = open(file_name, "a")
+    out_file.write(line)
+    out_file.close()
+
+
+def watts_generated():
+    url = "http://" + host + "/solar_api/v1/GetInverterRealtimeData.cgi?Scope=System"
+    r = requests.get(url, timeout=2)
+    json_data = r.json()
+    result = json_data["Body"]["Data"]["PAC"]["Values"]["1"]
+    return result
+
+
+if __name__ == "__main__":
+    main()
