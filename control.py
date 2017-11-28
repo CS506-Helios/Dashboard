6252d0b35da02f1a1e49cf02b12c4bb35b46f8b4
benklonowski <klonowskiben@gmail.com>
2017-11-28 15:24:34 -0600

    Added SQLalchemy for querying

--- control.py
+++ control.py
@@ -1,33 +1,66 @@
 import manipulator
-import sqlalchemy
+import datetime
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 class Controller:
 
-    engine = create_engine('helios-wei-dashboard.cq6hbz3m95ou.us-east-1.rds.amazonaws.com')
-    # The above string argument is the name of the server
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+
 
     # Query SQL database for data in the given timescale.
     def get_data (timescale):
+        engine = create_engine('helios-wei-dashboard.cq6hbz3m95ou.us-east-1.rds.amazonaws.com')
+        # The above string argument is the name of the server
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
         '''
         Given the new timescale (year, month, week, or day) create a query to the database to retrieve the data for the
         specified period
         '''
+
         if timescale == 'week':
+
+            now = datetime.datetime.now()
+            day = now.date()
             #SQL query to get data for the last 7 days
+            for entry in session.query(func.sum('TOTAL KWH')'TIMESTAMP'):
+                totals = [None] * 7
+                index = now.day() - 'TIMESTAMP DAY OF MONTH'
+                if index <= 7 & index > 0:
+                    totals[index] += 0 #TODO: replace with the KWH for the entry
+                print(entry.field1 + ' ' + entry.field2)
+
+
             print 'week query'
             return
+
         if timescale == 'month':
+
+            now = datetime.datetime.now()
+            current_month = now.month
+            for entry in session.query(func.sum('KWH')).filter(month = current_month):
+                totals = [None] * now.date()
+                totals['ENTRY DATE TIMESTAMP' + 1] += 0 #TODO: replace with the KWH for the entry
+                #Transmit data in JSON format to AJAX frontend
+
             print 'month query'
             # SQL query to request data for the month starting at the first of the month.
             return
+
         if timescale == 'year':
+            now = datetime.datetime.now()
+            year = now.year
+
             # SQL query to request data from the past 365 days
+            for entry in session.query('TIMESTAMP').filter('TIMESTAMP YEAR' == year):
+                #Transmit data in JSON format to AJAX front end
+                totals = [None] * 12
+                totals['ENTRY TIMESTAMP MONTH'+1] += 0 #TODO: replace with the KWH for the entry
+
             print 'year query'
             return
+
         if timescale == None:
             return
         #Call should be made to update_total
