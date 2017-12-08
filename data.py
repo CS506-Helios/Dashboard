5e27e3479205a5547af65ac34f17bf0b3d22fd68
benklonowski <klonowskiben@gmail.com>
2017-12-08 16:32:15 -0600

    Test suite

--- data.py
+++ data.py
@@ -8,27 +8,29 @@ cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';
 cursor = cnxn.cursor()
 #TO DO: Build and return an array which stores the energy value at each row at a separate index in the array
 def individial_energy_totals(energy_data):
-    totals = [None] * cursor.rowcount()
-    row = cursor.fetchone()
-    totals[cursor.rownumber()] = row
+    totals = [] * energy_data.rownumber
+    row = energy_data.fetchone()
+    while row:
+        totals[cursor.rownumber()] = row[0]
+        row = cursor.fetchone()
     return totals
 
 #TO DO: Return the sum of the values at each index of the passed array
 def total_energy(individial_energy_totals):
     total = 0
-    for sub_total in individial_energy_totals():
+    for sub_total in individial_energy_totals:
         total += sub_total
     return total
 
 #TO DO: Return value obtained by dividing the total energy by the energy unit
 def energy_analogy_value(total_energy, energy_unit):
-    return total_energy() / energy_unit
+    return total_energy / energy_unit
 
 #TO DO: Given the total energy produced and the price per KWh, return the money saved
 def money_saved(total_energy, price_per_kwh):
-    return total_energy() * price_per_kwh
+    return total_energy * price_per_kwh
 
 #TO DO: Given the total energy produced and the timescale, use the global variable to calculate
 #what percentage of the building's total energy consumption is accounted for by the panels
-def energy_percentage(total_energy, timescale, days):
+def energy_percentage(total_energy, days):
     return total_energy / (daily_energy_consumption * days)
