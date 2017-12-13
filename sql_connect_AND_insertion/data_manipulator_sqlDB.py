d126e2f8fd333baa16230bf93a4a1d0b1fa78071
Jiwon-N <33268337+Jiwon-N@users.noreply.github.com>
2017-12-12 19:29:09 -0600

    Update data_manipulator_sqlDB.py

--- sql_connect_AND_insertion/data_manipulator_sqlDB.py
+++ sql_connect_AND_insertion/data_manipulator_sqlDB.py
@@ -1,45 +1,68 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import MySQLdb
-
-#csv_parse.py
-import csv
+import requests
 from datetime import datetime
-with open('2017_12_04.csv') as csvfile:
-  reader = csv.reader(csvfile)
-  total = 0
-  for row in reader:
-      print(row[0])
-      print(row[1])
-      try:
-          current = float(row[1]) #this is the kwh value of the current row
-          date_time = datetime.strptime(row[0], '%m/%d/%Y %I:%M%p')
-      except:
-          print
+from decimal import *
+import time
 
-#Connecting to SQLdb
-conn = MySQLdb.connect(host = 'weiheliosdashboard.cq6hbz3m95ou.us-east-1.rds.amazonaws.com', 
-                               user = 'helios', passwd = 'cleanenergy123')
-cursor = conn.cursor()
-   
-if conn.is_connected():
-   print('Successfully connected to SQL database. Initiating data insertion.')
-   dataInsert()
-else:
-   print('Failed to connect to SQL database. Please try again.')
+# IP address of inverter
+host = "144.92.98.92"
+# number of seconds between samples
+sample_seconds = 60 * 5
+
+    
+
+import pyodbc
+
+#SQL server connection
+server = 'weiheliosdashboard.cq6hbz3m95ou.us-east-1.rds.amazonaws.com'
+database = 'WEIHeliosDashboard'
+username = 'helios'
+password = 'cleanenergy123'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
 
 
-#Inserting scraped and parsed data into SQLdb
-def dataInsert():
-   try:
-      cursor.execute("""INSERT INTO Energy (datetime, EnrgyInterval) VALUES ()""", (row[0], row[1]))
-      conn.commit()
-      cursor.execute("""SELECT * FROM Energy;""")
-      print cursor.fetchall()
-      
-   except:
-      conn.rollback()
-      
-if __name__ == '__main__':
-   dataInsert()
-   conn.close()
+
+def main():
+    energy_generated()
+    EnergyData_insertion()
+
+#Inserting scraped solar energy data
+def EnergyData_insertion():
+    print("started")
+    while True:
+        try:
+            energy = energy_generated()
+
+            now = time.strftime('%Y-%m-%d %H:%M:%S')
+            cursor.execute('''insert into EnergyData values(? , ?)''', (now, energy))
+            cnxn.commit()
+            cursor.execute('''select * from EnergyData;''')
+            results = cursor.fetchall()
+
+            for row in results:
+                print (row[0])
+    
+        except requests.exceptions.ConnectTimeout:
+            print("Connect timeout at %s" % time.strftime("%H:%M:%S"))
+        if sample_seconds > 0:
+            time.sleep(sample_seconds)
+        else:
+            return
+
+
+
+# power generated in certain time piriod
+def energy_generated():
+    url = "http://" + host + "/solar_api/v1/GetInverterRealtimeData.cgi?Scope=System"
+    r = requests.get(url, timeout=10)
+    json_data = r.json()
+    dic = json_data["Body"]["Data"]["PAC"]["Values"]
+    # sum up values in two units of inverters and transfer the unit to kWh
+    result = sum(dic.values()) / float(12000)
+    return result
+
+
+if __name__ == "__main__":
+    main()
